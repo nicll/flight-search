8ef5e8958675cd3358aa8b1b2392d53563c14cd2
Stefan <simanek.s@gmx.at>
2024-03-03 21:07:36 +0100

    init + KafkaProducerController done

---
+++ Flight-Search-Service/Flight-Search-Service/Controllers/KafkaProducerController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Confluent.Kafka;
+
+namespace Flight_Search_Service.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class KafkaProducerController : ControllerBase
+{
+    private readonly ProducerConfig _producerConfig;
+    public KafkaProducerController()
+    {
+        _producerConfig = new ProducerConfig
+        {
+            BootstrapServers = "localhost:9092"
+        };
+    }
+    [HttpPost("send/{topicName}")]
+    public IActionResult SendMessage(string topicName, [FromBody] string message)
+    {
+        try
+        {
+            using var producer = new ProducerBuilder<Null, string>(_producerConfig).Build();
+            producer.Produce(topicName, new Message<Null, string> { Value = message });
+            producer.Flush(TimeSpan.FromSeconds(10));
+            return Ok("Message sent successfully");
+        }
+        catch (Exception ex)
+        {
+            return StatusCode(500, $"An error occurred: {ex.Message}");
+        }
+    }
+}
