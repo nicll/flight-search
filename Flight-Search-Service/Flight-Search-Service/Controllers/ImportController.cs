50933dfe1233d28f35572702bb5c8570260282e7
Stefan <simanek.s@gmx.at>
2024-03-17 22:24:29 +0100

    ImportController done

---
+++ Flight-Search-Service/Flight-Search-Service/Controllers/ImportController.cs
@@ -0,0 +1,57 @@
+﻿using Confluent.Kafka;
+using Flight_Search_Service.Util;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace Flight_Search_Service.Controllers;
+
+[ApiController]
+[Route("api/Import")]
+public class ImportController : ControllerBase
+{
+    private readonly string _endpoint = "http://api.aviationstack.com/v1/flights?access_key="; //missing API Key
+    private readonly string _topicName = ""; //missing topic name
+    private readonly HttpClient _httpClient;
+    private readonly IProducer<string, string> _kafkaProducer;
+    public ImportController(IHttpClientFactory httpClientFactory)
+    {
+        _httpClient = httpClientFactory.CreateClient();
+        var config = new ProducerConfig
+        {
+            BootstrapServers = "localhost:9092"
+        };
+        _kafkaProducer = new ProducerBuilder<string, string>(config).Build();
+    }
+    [HttpGet("ImportAviationStackData")]
+    public async Task<IActionResult> ImportAviationStackData()
+    {
+        try
+        {
+            HttpResponseMessage response = await _httpClient.GetAsync(_endpoint);
+            if (response.IsSuccessStatusCode)
+            {
+                var jsonContent = await response.Content.ReadAsStringAsync();
+                List<Flight> flightDataList = JsonConvert.DeserializeObject<FlightDataWrapper>(jsonContent)!.Data;
+                foreach (var flight in flightDataList)
+                {
+                    var flightJson = JsonConvert.SerializeObject(flight);
+                    var message = new Message<string, string>
+                    {
+                        Key = null,
+                        Value = flightJson
+                    };
+                    await _kafkaProducer.ProduceAsync(_topicName, message);
+                }
+                return Ok("Flight data sent to Kafka successfully");
+            }
+            else
+            {
+                return StatusCode((int)response.StatusCode, "Failed to fetch flight data");
+            }
+        }
+        catch (Exception ex)
+        {
+            return StatusCode(500, $"An error occurred: {ex.Message}");
+        }
+    }
+}
---
+++ Flight-Search-Service/Flight-Search-Service/Util/AviationStackJsonClasses.cs
@@ -0,0 +1,76 @@
+﻿namespace Flight_Search_Service.Util;
+
+public class Departure
+{
+    public string Airport { get; set; }
+    public string Timezone { get; set; }
+    public string Iata { get; set; }
+    public string Icao { get; set; }
+    public object Terminal { get; set; }
+    public string Gate { get; set; }
+    public object Delay { get; set; }
+    public DateTime Scheduled { get; set; }
+    public DateTime Estimated { get; set; }
+    public object Actual { get; set; }
+    public object Estimated_runway { get; set; }
+    public object Actual_runway { get; set; }
+}
+
+public class Arrival
+{
+    public string Airport { get; set; }
+    public string Timezone { get; set; }
+    public string Iata { get; set; }
+    public string Icao { get; set; }
+    public string Terminal { get; set; }
+    public object Gate { get; set; }
+    public object Baggage { get; set; }
+    public object Delay { get; set; }
+    public DateTime Scheduled { get; set; }
+    public DateTime Estimated { get; set; }
+    public object Actual { get; set; }
+    public object Estimated_runway { get; set; }
+    public object Actual_runway { get; set; }
+}
+
+public class Airline
+{
+    public string Name { get; set; }
+    public string Iata { get; set; }
+    public string Icao { get; set; }
+}
+
+public class Codeshared
+{
+    public string Airline_name { get; set; }
+    public string Airline_iata { get; set; }
+    public string Airline_icao { get; set; }
+    public string Flight_number { get; set; }
+    public string Flight_iata { get; set; }
+    public string Flight_icao { get; set; }
+}
+
+public class Flight
+{
+    public string Flight_date { get; set; }
+    public string Flight_status { get; set; }
+    public Departure Departure { get; set; }
+    public Arrival Arrival { get; set; }
+    public Airline Airline { get; set; }
+    public FlightInfo FlightInfo { get; set; }
+    public object Aircraft { get; set; }
+    public object Live { get; set; }
+}
+
+public class FlightInfo
+{
+    public string Number { get; set; }
+    public string Iata { get; set; }
+    public string Icao { get; set; }
+    public Codeshared Codeshared { get; set; }
+}
+
+public class FlightDataWrapper
+{
+    public List<Flight> Data { get; set; }
+}
