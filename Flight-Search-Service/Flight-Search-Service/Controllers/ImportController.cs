e1fb38dd36398985a916fef9ebe24b22257f7c31
Stefan <simanek.s@gmx.at>
2024-04-17 18:07:14 +0200

    ImportOpenskynetworkData done

--- Flight-Search-Service/Flight-Search-Service/Controllers/ImportController.cs
+++ Flight-Search-Service/Flight-Search-Service/Controllers/ImportController.cs
@@ -9,7 +9,8 @@ namespace Flight_Search_Service.Controllers;
 [Route("api/Import")]
 public class ImportController : ControllerBase
 {
-    private readonly string _endpoint = "http://api.aviationstack.com/v1/flights?access_key="; //missing API Key
+    private readonly string _aviationstackEndpoint = "http://api.aviationstack.com/v1/flights?access_key="; //missing API Key
+    private readonly string _openskynetworkEndpoint = "https://opensky-network.org/api/states/all";
     private readonly string _topicName = ""; //missing topic name
     private readonly HttpClient _httpClient;
     private readonly IProducer<string, string> _kafkaProducer;
@@ -27,11 +28,14 @@ public class ImportController : ControllerBase
     {
         try
         {
-            HttpResponseMessage response = await _httpClient.GetAsync(_endpoint);
+            HttpResponseMessage response = await _httpClient.GetAsync(_aviationstackEndpoint);
             if (response.IsSuccessStatusCode)
             {
                 var jsonContent = await response.Content.ReadAsStringAsync();
                 List<Flight> flightDataList = JsonConvert.DeserializeObject<FlightDataWrapper>(jsonContent)!.Data;
+
+
+
                 foreach (var flight in flightDataList)
                 {
                     var flightJson = JsonConvert.SerializeObject(flight);
@@ -53,5 +57,71 @@ public class ImportController : ControllerBase
         {
             return StatusCode(500, $"An error occurred: {ex.Message}");
         }
+    }
+    [HttpGet("ImportOpenskynetworkData")]
+    public async Task<IActionResult> ImportOpenskynetworkData()
+    {
+        try
+        {
+            HttpResponseMessage response = await _httpClient.GetAsync(_openskynetworkEndpoint);
+            if (response.IsSuccessStatusCode)
+            {
+                var jsonContent = await response.Content.ReadAsStringAsync();
+                dynamic jsonObject = JsonConvert.DeserializeObject(jsonContent)!;
+                var statesArray = jsonObject.states;
+                List<State> statesList = DeserializeStates(statesArray);
+
+
+
+                foreach (var state in statesList)
+                {
+                    var flightJson = JsonConvert.SerializeObject(state);
+                    var message = new Message<string, string>
+                    {
+                        Key = null,
+                        Value = flightJson
+                    };
+                    await _kafkaProducer.ProduceAsync(_topicName, message);
+                }
+                return Ok(statesList.First());
+            }
+            else
+            {
+                return StatusCode((int)response.StatusCode, "Failed to fetch flight data");
+            }
+        }
+        catch (Exception ex)
+        {
+            return StatusCode(500, $"An error occurred: {ex}");
+        }
+    }
+    private static List<State> DeserializeStates(dynamic statesArray)
+    {
+        List<State> states = [];
+        foreach (var stateArray in statesArray)
+        {
+            State state = new()
+            {
+                Icao24 = stateArray[0],
+                Callsign = stateArray[1],
+                OriginCountry = stateArray[2],
+                TimePosition = stateArray[3] ?? null,
+                LastContact = stateArray[4] ?? null,
+                Longitude = stateArray[5],
+                Latitude = stateArray[6],
+                BaroAltitude = stateArray[7],
+                OnGround = stateArray[8],
+                Velocity = stateArray[9],
+                TrueTrack = stateArray[10],
+                VerticalRate = stateArray[11],
+                Sensors = stateArray[12],
+                GeoAltitude = stateArray[13],
+                Squawk = stateArray[14],
+                Spi = stateArray[15],
+                PositionSource = stateArray[16]
+            };
+            states.Add(state);
+        }
+        return states;
     }
 }
---
+++ Flight-Search-Service/Flight-Search-Service/Util/OpenskynetworkClasses.cs
@@ -0,0 +1,29 @@
+﻿namespace Flight_Search_Service.Util;
+
+public class State
+{
+    public string? Icao24 { get; set; }
+    public string? Callsign { get; set; }
+    public string? OriginCountry { get; set; }
+    public long? TimePosition { get; set; }
+    public long? LastContact { get; set; }
+    public float? Longitude { get; set; }
+    public float? Latitude { get; set; }
+    public float? BaroAltitude { get; set; }
+    public bool OnGround { get; set; }
+    public float? Velocity { get; set; }
+    public float? TrueTrack { get; set; }
+    public float? VerticalRate { get; set; }
+    public double[]? Sensors { get; set; }
+    public float? GeoAltitude { get; set; }
+    public string? Squawk { get; set; }
+    public bool Spi { get; set; }
+    public double? PositionSource { get; set; }
+}
+
+public class RootObject
+{
+    public int? Time { get; set; }
+    public List<State> States { get; set; }
+}
+
